023454f5f0f1d2bd82856adb48bf41c9311499ab
d33d33 <kevin@d33d33.fr>
2018-02-26 18:17:53 +0100

    feat(sink): use only one reactor per sink

--- src/sink/mod.rs
+++ src/sink/mod.rs
@@ -4,9 +4,16 @@
 use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use hyper;
+use futures::sync::oneshot;
+use futures::Future;
+use futures::future::Shared;
+use hyper_tls::HttpsConnector;
+use hyper_timeout::TimeoutConnector;
+use std::time::Duration;
+use futures::sync::mpsc::{channel, Sender};
+use futures::task::Task;
 
 use slog_scope;
 
@@ -19,7 +26,6 @@ pub struct Sink<'a> {
     name: &'a String,
     todo: Arc<Mutex<VecDeque<PathBuf>>>,
     handles: Vec<thread::JoinHandle<()>>,
-    sigint: Arc<AtomicBool>,
     dir: &'a String,
     watch_period: u64,
     timeout: u64,
@@ -32,15 +38,18 @@ pub struct Sink<'a> {
     max_size: u64,
     ttl: u64,
     parallel: u64,
+    sigint: (oneshot::Sender<()>, Shared<oneshot::Receiver<()>>),
 }
 
 impl<'a> Sink<'a> {
     pub fn new(sink: &'a config::Sink, parameters: &'a config::Parameters) -> Sink<'a> {
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+
         Sink {
             name: &sink.name,
             todo: Arc::new(Mutex::new(VecDeque::new())),
             handles: Vec::new(),
-            sigint: Arc::new(AtomicBool::new(false)),
+            sigint: (shutdown_tx, shutdown_rx.shared()),
             dir: &parameters.sink_dir,
             watch_period: parameters.scan_period,
             timeout: parameters.timeout,
@@ -57,60 +66,87 @@ impl<'a> Sink<'a> {
     }
 
     pub fn start(&mut self) {
-        slog_scope::scope(
-            &slog_scope::logger().new(o!("sink" => self.name.clone())),
-            || {
-                // spawn fs thread
-                let (name, dir, period, todo, sigint) = (
-                    self.name.clone(),
-                    self.dir.clone(),
-                    self.watch_period,
-                    self.todo.clone(),
-                    self.sigint.clone(),
-                );
-                let (max_size, ttl) = (self.max_size, self.ttl);
+        debug!("start sink: {}", self.name);
+        let (notify_tx, notify_rx) = channel(self.parallel as usize); // TODO move to unsync one
 
-                self.handles.push(thread::spawn(move || {
-                    slog_scope::scope(
-                        &slog_scope::logger().new(o!("sink" => name.clone())),
-                        || fs::fs_thread(&name, &dir, period, todo, max_size, ttl, sigint),
-                    );
-                }));
-
-                // spawn send threads
-                for _ in 0..self.parallel {
-                    let (name, sigint) = (self.name.clone(), self.sigint.clone());
-                    let todo = self.todo.clone();
-                    let url = self.url.clone();
-                    let (timeout, keep_alive) = (self.timeout, self.keep_alive);
-                    let (token, token_header) = (self.token.clone(), self.token_header.clone());
-                    let (batch_count, batch_size) = (self.batch_count, self.batch_size);
-
-                    self.handles.push(thread::spawn(move || {
-                        slog_scope::scope(
-                            &slog_scope::logger().new(o!("sink" => name.clone())),
-                            || {
-                                send::send_thread(
-                                    &token,
-                                    &token_header,
-                                    url,
-                                    todo,
-                                    timeout,
-                                    keep_alive,
-                                    batch_count,
-                                    batch_size,
-                                    sigint,
-                                )
-                            },
-                        );
-                    }));
-                }
-            },
+        // spawn fs thread
+        let (name, dir, period, todo, sigint) = (
+            self.name.clone(),
+            self.dir.clone(),
+            self.watch_period,
+            self.todo.clone(),
+            self.sigint.1.clone(),
         );
+        let (max_size, ttl) = (self.max_size, self.ttl);
+
+        self.handles.push(thread::spawn(move || {
+            slog_scope::scope(
+                &slog_scope::logger().new(o!("sink" => name.clone())),
+                || fs::fs_thread(&name, &dir, period, todo, max_size, ttl, sigint, notify_rx),
+            );
+        }));
+
+        let (name, sigint) = (self.name.clone(), self.sigint.1.clone());
+        let todo = self.todo.clone();
+        let url = self.url.clone();
+        let (timeout, keep_alive) = (self.timeout, self.keep_alive);
+        let (token, token_header) = (self.token.clone(), self.token_header.clone());
+        let (batch_count, batch_size) = (self.batch_count, self.batch_size);
+        let parallel = self.parallel.clone();
+
+        self.handles.push(thread::spawn(move || {
+            slog_scope::scope(
+                &slog_scope::logger().new(o!("sink" => name.clone())),
+                || {
+                    let mut core =
+                        ::tokio_core::reactor::Core::new().expect("Fail to start tokio reactor");
+                    let handle = core.handle();
+                    let connector =
+                        HttpsConnector::new(4, &handle).expect("Fail to start https connector");
+
+                    // Handle connection timeouts
+                    let mut tm = TimeoutConnector::new(connector, &handle);
+                    tm.set_connect_timeout(Some(Duration::from_secs(timeout)));
+                    tm.set_read_timeout(Some(Duration::from_secs(timeout)));
+                    tm.set_write_timeout(Some(Duration::from_secs(timeout)));
+
+                    // Client should be shared accross sinks
+                    let client = Arc::new(
+                        hyper::Client::configure()
+                            .body::<send::PayloadBody>()
+                            .keep_alive(keep_alive)
+                            .connector(tm)
+                            .build(&handle),
+                    );
+
+                    // spawn send threads
+                    for _ in 0..parallel {
+                        let client = client.clone();
+                        let (token, token_header) = (token.clone(), token_header.clone());
+                        let (url, todo) = (url.clone(), todo.clone());
+                        let notify_tx: Sender<Task> = notify_tx.clone();
+
+                        let work = send::send_thread(
+                            client,
+                            token,
+                            token_header,
+                            url,
+                            todo,
+                            batch_count,
+                            batch_size,
+                            notify_tx,
+                        );
+
+                        core.handle().spawn(work);
+                    }
+                    core.run(sigint).expect("Sigint could not fail");
+                },
+            )
+        }));
     }
 
     pub fn stop(self) {
-        self.sigint.store(true, Ordering::SeqCst);
+        self.sigint.0.send(()).unwrap();
         for handle in self.handles {
             handle.join().unwrap();
         }
--- src/sink/send.rs
+++ src/sink/send.rs
@@ -1,135 +1,49 @@
 use std::collections::VecDeque;
-use std::error::Error;
 use std::fs;
-use std::fmt;
-use std::error;
 use std::fs::File;
-use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::ops::Deref;
+use std::ops::DerefMut;
+
 use hyper;
 use hyper_tls::HttpsConnector;
 use hyper_timeout::TimeoutConnector;
+
 use futures::{Async, Future, Poll, Stream};
-use futures::future::{err, ok};
-use std::time::Duration;
-use std::ops::Deref;
-use std::ops::DerefMut;
+use futures::future::{ok, FutureResult};
+use futures::sync::mpsc::Sender;
+use futures::task::{current, Task};
 // use flate2::Compression;
 // use flate2::write::ZlibEncoder;
 
-use config;
-
 const CHUNK_SIZE: usize = 1024 * 1024;
 
-#[derive(Debug)]
-enum SendError {
-    Io(io::Error),
-    Format(Box<Error>),
-    Hyper(hyper::Error),
-}
-
-impl From<io::Error> for SendError {
-    fn from(err: io::Error) -> SendError {
-        SendError::Io(err)
-    }
-}
-impl From<Box<Error>> for SendError {
-    fn from(err: Box<Error>) -> SendError {
-        SendError::Format(err)
-    }
-}
-impl<'a> From<&'a str> for SendError {
-    fn from(err: &str) -> SendError {
-        SendError::Format(From::from(err))
-    }
-}
-impl From<String> for SendError {
-    fn from(err: String) -> SendError {
-        SendError::Format(From::from(err))
-    }
-}
-impl From<hyper::Error> for SendError {
-    fn from(err: hyper::Error) -> SendError {
-        SendError::Hyper(err)
-    }
-}
-
-impl fmt::Display for SendError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            SendError::Io(ref err) => err.fmt(f),
-            SendError::Format(ref err) => err.fmt(f),
-            SendError::Hyper(ref err) => err.fmt(f),
-        }
-    }
-}
-
-impl error::Error for SendError {
-    fn description(&self) -> &str {
-        match *self {
-            SendError::Io(ref err) => err.description(),
-            SendError::Format(ref err) => err.description(),
-            SendError::Hyper(ref err) => err.description(),
-        }
-    }
-
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            SendError::Io(ref err) => Some(err),
-            SendError::Format(ref err) => Some(err.as_ref()),
-            SendError::Hyper(ref err) => Some(err),
-        }
-    }
-}
-
-enum HttpResult {
-    Error(SendError),
-    Ok((hyper::StatusCode, String)),
-}
-
 pub fn send_thread(
-    token: &str,
-    token_header: &str,
+    client: Arc<
+        hyper::Client<TimeoutConnector<HttpsConnector<hyper::client::HttpConnector>>, PayloadBody>,
+    >,
+    token: String,
+    token_header: String,
     url: hyper::Uri,
     todo: Arc<Mutex<VecDeque<PathBuf>>>,
-    timeout: u64,
-    keep_alive: bool,
     batch_count: u64,
     batch_size: u64,
-    sigint: Arc<AtomicBool>,
-) {
-    let mut core = ::tokio_core::reactor::Core::new().expect("Fail to start tokio reactor");
-    let handle = core.handle();
-    let connector = HttpsConnector::new(4, &handle).expect("Fail to start https connector");
+    notify_tx: Sender<Task>,
+) -> Box<Future<Item = (), Error = ()>> {
+    let work = PayloadStream::new(todo, batch_count, batch_size, notify_tx)
+        .for_each(move |mut p| {
+            let mut req: hyper::Request<PayloadBody> =
+                hyper::Request::new(hyper::Post, url.clone());
+            req.set_body(p.body().expect("Body is never None"));
+            req.headers_mut()
+                .set_raw(token_header.clone(), token.clone());
 
-    // Handle connection timeouts
-    let mut tm = TimeoutConnector::new(connector, &handle);
-    tm.set_connect_timeout(Some(Duration::from_secs(timeout)));
-    tm.set_read_timeout(Some(Duration::from_secs(timeout)));
-    tm.set_write_timeout(Some(Duration::from_secs(timeout)));
-
-    // Client should be shared accross sinks
-    let client = hyper::Client::configure()
-        .body::<Payload>()
-        .keep_alive(keep_alive)
-        .connector(tm)
-        .build(&handle);
-
-    loop {
-        Payload::new(sigint.clone(), todo.clone(), batch_count, batch_size).map(|p| {
-            let processed = p.processed();
-
-            let mut req: hyper::Request<Payload> = hyper::Request::new(hyper::Post, url.clone());
-            req.set_body(p);
-            req.headers_mut().set_raw(String::from(token_header), token);
-
-            info!("Req");
             let req = client
+                .clone()
                 .request(req)
                 .and_then(|res| {
                     let status = res.status();
@@ -143,107 +57,164 @@ pub fn send_thread(
                         })
                         .or_else(|_| ok(String::new())); // Default body as an empty string
 
-                    ok(status)
-                        .join(body)
-                        .and_then(|res| ok(HttpResult::Ok(res)))
+                    ok(status).join(body)
                 })
-                .or_else(|err| ok(HttpResult::Error(SendError::Hyper(err))));
-
-            core.run(req.then(|res: Result<HttpResult, Box<Error>>| match res {
-                Err(error) => {
-                    crit!("unrecoverable error: {}", error);
-                    err(())
-                }
-                Ok(res) => {
+                .then(move |res| -> FutureResult<(), ()> {
                     let state = match res {
-                        HttpResult::Ok(result) => {
-                            let status = result.0;
+                        Err(error) => {
+                            error!("HTTP error: {}", error);
+                            Err(())
+                        }
+                        Ok(res) => {
+                            let status = res.0;
+                            let body = res.1;
                             if status.is_success() {
+                                debug!("{}", body);
                                 Ok(())
                             } else {
                                 error!("HTTP error: {}", status);
+                                debug!("{}", body);
                                 Err(())
                             }
-                        }
-                        HttpResult::Error(error) => {
-                            error!("HTTP error: {}", error);
-                            Err(())
                         }
                     };
 
                     match state {
                         Err(_) => {
-                            // recover processing file
-                            let mut processed = processed.lock().unwrap();
-                            let mut todo = todo.lock().unwrap();
-                            for entry in processed.deref_mut().drain(0..) {
-                                debug!("pushback sink file {}", format!("{:?}", entry.display()));
-                                todo.push_front(entry);
-                            }
+                            // recover processed file
+                            p.abort();
                         }
                         Ok(_) => {
-                            info!("post success");
-                            // info!("post success - {}", payload.sent_lines);
-                            // debug!("{}", b);
-                            let processed = processed.lock().unwrap();
-                            for entry in processed.deref() {
-                                debug!("delete sink file {}", format!("{:?}", entry.display()));
-                                match fs::remove_file(entry) {
-                                    Err(err) => error!("{}", err),
-                                    Ok(()) => {}
-                                }
-                            }
+                            let sent = p.commit();
+                            info!("post success - {}", sent);
                         }
                     };
                     ok(())
-                }
-            }))
-        });
+                });
 
-        thread::sleep(Duration::from_millis(config::REST_TIME));
-        if sigint.load(Ordering::Relaxed) {
-            return;
+            req
+        })
+        .then(|_| ok(()));
+
+    return Box::new(work);
+}
+
+struct PayloadStream {
+    batch_count: u64,
+    batch_size: u64,
+    todo: Arc<Mutex<VecDeque<PathBuf>>>,
+    notify_tx: Sender<Task>,
+}
+
+impl PayloadStream {
+    pub fn new(
+        todo: Arc<Mutex<VecDeque<PathBuf>>>,
+        batch_count: u64,
+        batch_size: u64,
+        notify_tx: Sender<Task>,
+    ) -> PayloadStream {
+        PayloadStream {
+            batch_count: batch_count,
+            batch_size: batch_size,
+            todo: todo,
+            notify_tx: notify_tx,
+        }
+    }
+}
+
+impl Stream for PayloadStream {
+    type Item = Payload;
+    type Error = ();
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        match self.todo.lock().unwrap().pop_front() {
+            Some(path) => {
+                let s = Payload::new(self.todo.clone(), self.batch_count, self.batch_size, path);
+                Ok(Async::Ready(Some(s)))
+            }
+            None => {
+                self.notify_tx
+                    .try_send(current())
+                    .expect("send could never failed");
+                Ok(Async::NotReady)
+            }
         }
     }
 }
 
 struct Payload {
-    batch_count: u64,
-    batch_size: i64,
-    sigint: Arc<AtomicBool>,
     todo: Arc<Mutex<VecDeque<PathBuf>>>,
-    processing: Option<PathBuf>,
     processed: Arc<Mutex<Vec<PathBuf>>>,
-    reader: Option<BufReader<File>>,
-    remaining: String,
-    sent_lines: usize,
+    sent_lines: Arc<AtomicUsize>,
+    body: Option<PayloadBody>,
 }
 
 impl Payload {
     pub fn new(
-        sigint: Arc<AtomicBool>,
         todo: Arc<Mutex<VecDeque<PathBuf>>>,
         batch_count: u64,
         batch_size: u64,
-    ) -> Option<Payload> {
-        let mut p = Payload {
-            sigint: sigint,
-            batch_count: batch_count,
-            batch_size: batch_size as i64,
+        file: PathBuf,
+    ) -> Payload {
+        let mut body = PayloadBody::new(todo.clone(), batch_count, batch_size);
+        body.load(Some(file));
+        Payload {
             todo: todo,
-            processing: None,
+            processed: body.processed(),
+            sent_lines: body.sent_lines(),
+            body: Some(body),
+        }
+    }
+
+    fn body(&mut self) -> Option<PayloadBody> {
+        self.body.take()
+    }
+
+    fn abort(&mut self) {
+        let mut processed = self.processed.lock().unwrap();
+        let mut todo = self.todo.lock().unwrap();
+        for entry in processed.deref_mut().drain(0..) {
+            debug!("pushback sink file {}", format!("{:?}", entry.display()));
+            todo.push_back(entry);
+        }
+    }
+
+    fn commit(&mut self) -> usize {
+        let processed = self.processed.lock().unwrap();
+        for entry in processed.deref() {
+            debug!("delete sink file {}", format!("{:?}", entry.display()));
+            match fs::remove_file(entry) {
+                Err(err) => error!("{}", err),
+                Ok(()) => {}
+            }
+        }
+
+        self.sent_lines.load(Ordering::Relaxed)
+    }
+}
+
+pub struct PayloadBody {
+    remaining_count: u64,
+    remaining_size: i64,
+    todo: Arc<Mutex<VecDeque<PathBuf>>>,
+    processed: Arc<Mutex<Vec<PathBuf>>>,
+    reader: Option<BufReader<File>>,
+    sent_lines: Arc<AtomicUsize>,
+}
+
+impl PayloadBody {
+    pub fn new(
+        todo: Arc<Mutex<VecDeque<PathBuf>>>,
+        batch_count: u64,
+        batch_size: u64,
+    ) -> PayloadBody {
+        PayloadBody {
+            remaining_count: batch_count,
+            remaining_size: batch_size as i64,
+            todo: todo,
             processed: Arc::new(Mutex::new(Vec::new())),
             reader: None,
-            remaining: String::new(),
-            sent_lines: 0,
+            sent_lines: Arc::new(AtomicUsize::new(0)),
             // encoder: ZlibEncoder::new(Vec::new(), Compression::Default),
-        };
-
-        p.processing = p.get_file();
-
-        match p.processing {
-            Some(_) => Some(p),
-            None => None,
         }
     }
 
@@ -251,98 +222,73 @@ impl Payload {
         self.processed.clone()
     }
 
-    fn get_file(&mut self) -> Option<PathBuf> {
-        let mut todo = self.todo.lock().unwrap();
-        let file = todo.pop_front();
-
-        match file {
-            Some(ref f) => self.processed.lock().unwrap().push(f.clone()),
-            None => {}
-        }
-
-        file
+    fn sent_lines(&self) -> Arc<AtomicUsize> {
+        self.sent_lines.clone()
     }
 
-    fn load_file(&self, path: &PathBuf) -> Option<BufReader<File>> {
-        debug!("load file {}", path.display());
-        match File::open(path) {
-            Err(err) => {
-                error!("{}", err);
-                None
-            }
-            Ok(f) => Some(BufReader::new(f)),
+    fn try_load(&mut self) {
+        if self.remaining_count == 0 || self.remaining_size <= 0 {
+            self.reader = None;
+            return;
         }
+
+        let path = self.todo.lock().unwrap().pop_front();
+        self.load(path);
+    }
+
+    fn load(&mut self, path: Option<PathBuf>) {
+        self.reader = path.and_then(|path| {
+            debug!("load file {}", path.display());
+            self.processed.lock().unwrap().push(path.clone());
+
+            self.remaining_count = self.remaining_count - 1;
+
+            match File::open(path) {
+                Err(err) => {
+                    error!("{}", err);
+                    None
+                }
+                Ok(f) => Some(BufReader::new(f)),
+            }
+        })
     }
 }
 
-impl Stream for Payload {
+impl Stream for PayloadBody {
     type Item = hyper::Chunk;
     type Error = hyper::Error;
 
     fn poll(&mut self) -> Poll<Option<hyper::Chunk>, hyper::Error> {
-        // Abort on sigint
-        if self.sigint.load(Ordering::Relaxed) && self.processing.is_none() {
+        // Hover?
+        if self.reader.is_none() {
             return Ok(Async::Ready(None));
         }
 
-        // Get a file to process
-        if self.processing.is_none() && self.batch_count > 0 && self.batch_size > 0 {
-            self.processing = self.get_file();
-        }
-
-        // Load reader
-        if self.reader.is_none() && self.processing.is_some() {
-            let to_load = self.processing.as_ref().unwrap();
-            self.batch_count = self.batch_count - 1;
-            self.reader = self.load_file(to_load);
-        }
-
-        // Chunk by 1M or less
         let mut s = String::with_capacity(CHUNK_SIZE);
         let mut idx = 0;
-
-        // Process remaining data from previous chunk
-        if self.remaining.len() > 0 {
-            idx = self.remaining.len();
-            s.push_str(&self.remaining);
-            s.clear();
-        }
-
-        // if self.processing.is_some() {
-        //     Ok(Async::Ready(Some(Ok(hyper::Chunk::from(
-        //         self.load_file_as_bytes(self.processing.as_ref().unwrap())
-        //             .unwrap(),
-        //     )))))
-        // } else {
-        //     Ok(Async::Ready(None))
-        // }
+        let mut sent_lines = 0;
 
         // send file
-        let mut drained = false;
-        if idx < CHUNK_SIZE && self.reader.is_some() {
-            let r = self.reader.as_mut().unwrap();
-            while idx < CHUNK_SIZE {
-                let size = r.read_line(&mut s)?; // TODO handle chunk size overflow
-                if size == 0 {
-                    drained = true; // TODO continue to loop throught new file
-                    break;
+        while idx < CHUNK_SIZE {
+            let size = self.reader
+                .as_mut()
+                .expect("reader is never None")
+                .read_line(&mut s)?;
+            if size == 0 {
+                self.try_load();
+
+                if self.reader.is_none() {
+                    break; // No more work to do
                 }
+            } else {
                 idx = idx + size;
-                self.sent_lines = self.sent_lines + 1;
+                sent_lines = sent_lines + 1;
             }
         }
 
-        if drained {
-            self.reader = None;
-            self.processing = None;
-        }
+        self.sent_lines.fetch_add(sent_lines, Ordering::Relaxed);
 
-        self.batch_size = self.batch_size - idx as i64;
-
-        if s.len() > 0 {
-            Ok(Async::Ready(Some(hyper::Chunk::from(s))))
-        } else {
-            Ok(Async::Ready(None))
-        }
+        self.remaining_size = self.remaining_size - idx as i64;
+        Ok(Async::Ready(Some(hyper::Chunk::from(s))))
     }
 }
